203abe40b02489e9f674616e773a2496888670f5
yukihyogo <secondaryshrike@gmail.com>
2015-08-30 14:19:30 +0000

    check scroll

---
+++ script.js
@@ -0,0 +1,67 @@
+$(function(){
+    var box    = $("#menu-wrap");
+    var boxTop = box.offset().top;
+    var boxBottom = boxTop + box.height();    
+    var topBtn = $('#page-top');    
+    var topBtnTop = topBtn.offset().top;
+    topBtn.hide();
+    $(window).scroll(function () {
+        if($(window).scrollTop() >= topBtnTop - boxBottom) {
+            topBtn.fadeIn();
+            if($(window).scrollTop() >= boxTop) {
+                box.addClass("fixed");
+    			$("body").css("margin-top","40px");
+            } else {
+                box.removeClass("fixed");
+    			$("body").css("margin-top","0px");
+            }
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップに移動
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+
+
+/*
+$(function(){
+    var box    = $("#menu-wrap");
+    var boxTop = box.offset().top;
+    $(window).scroll(function () {
+        if($(window).scrollTop() >= boxTop) {
+            box.addClass("fixed");
+			$("body").css("margin-top","40px");
+        } else {
+            box.removeClass("fixed");
+			$("body").css("margin-top","0px");
+        }
+    });
+});
+
+$(function() {
+    var topBtn = $('#page-top');    
+    topBtn.hide();
+    //スクロールが指定値に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+*/
